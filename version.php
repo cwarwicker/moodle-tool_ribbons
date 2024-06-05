817f1899542ca7c8f6b2034d9718451c90c4770c
Conn Warwicker <conn@cmrwarwicker.com>
2024-06-05 13:24:28 +0100

    Bump version to v1.0.3

--- version.php
+++ version.php
@@ -25,7 +25,7 @@ defined('MOODLE_INTERNAL') || die;
 
 $plugin->component = 'tool_ribbons';
 $plugin->version  = 2021020402;
-$plugin->release = 'v1.0.2';
+$plugin->release = 'v1.0.3';
 $plugin->maturity = MATURITY_STABLE;
 $plugin->cron = 0;
 $plugin->requires = 2019052000;
